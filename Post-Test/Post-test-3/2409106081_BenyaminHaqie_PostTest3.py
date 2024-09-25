40d1d8633c42b2f1cfe2ee32fe49f98e7e79c6ad
haqi2 <qiee705@gmail.com>
2024-09-25 08:41:41 +0800

    Add files via upload

---
+++ Post-Test/Post-test-3/2409106081_BenyaminHaqie_PostTest3.py
@@ -0,0 +1,14 @@
+beratbadan = float(input("Masukkan berat badan: "))
+tinggibadan = float(input("Masukkan tinggi badan: "))
+tinggimeter = tinggibadan / 100
+total = beratbadan / (tinggimeter * tinggimeter)
+
+if total < 18.5 :
+    print ("bmi : Underweight") 
+elif total < 24.9 :
+    print ("bmi : Normal")
+elif total < 29.9 :
+    print ("overweight")
+else :
+    print ("obesitas")
+    
